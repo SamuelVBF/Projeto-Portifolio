93df455407dc227cb3e187ebccd4c38ce518b802
SamuelVBF <samuel.bjj@hotmail.com>
2022-02-22 20:41:46 -0300

    master

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -3,6 +3,6 @@
 const display = document.getElementById('display');
 const numeros = document.querySelectorAll('[id*=botao]');
 
-const inserirNumero = (evento) =>display.textContent = evento.target.textContent;
+const inserirNumero = (evento) => display.textContent = evento.target.textContent;
 
 numeros.forEach (numero => numero.addEventListener('click',inserirNumero));
