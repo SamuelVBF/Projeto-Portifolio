ada432029c1ff73bb926ea53d0ed88c0cf0540bf
SamuelVBF <samuel.bjj@hotmail.com>
2022-03-22 10:29:26 -0300

    'master'

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -2,7 +2,21 @@
 
 const display = document.getElementById('display');
 const numeros = document.querySelectorAll('[id*=botao]');
+const operadores = document.querySelectorAll('[id*=Operador]');
 
-const inserirNumero = (evento) => display.textContent = evento.target.textContent;
+let novoNumero = true;
 
+const atualizarDisplay = (texto) => {
+    if (novoNumero){
+        display.textContent = texto;
+        novoNumero = false;
+    }else{
+        display.textContent += texto;
+    }
+}
+
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);  
 numeros.forEach (numero => numero.addEventListener('click',inserirNumero));
+
+
+operadores.forEach (operador => operador.addEventListener('click',selecionarOperador));
